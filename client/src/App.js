46b0000118babf7196a552cbeb21d67293ec8ddb
Emily Yost <esyost@gmail.com>
2017-11-18 15:06:00 -0700

    small map

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,16 @@ import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FishMap from './components/map/Map';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Fishing App</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+     
+       <FishMap />
+       
+    )}
 }
 
 export default App;
--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
+// import './Map.css';
+import { Map } from 'react-arcgis';
 
-
-
-class Map extends Component {
+class FishMap extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Fishing App</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+       <Map />
+    )}
 }
 
-export default Map;
+export default FishMap;
