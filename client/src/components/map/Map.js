7ab0f2ba8491b9af92548f38ba1fc181d38f70ef
Emily Yost <esyost@gmail.com>
2017-11-18 17:26:35 -0700

    pop-up

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -26,14 +26,63 @@ class FishMap extends Component {
                   var fishMap = new Map (
                     {basemap: 'topo'}
                   )
-                  new MapView({
+                  var fishView = new MapView({
                       container: containerNode,
                       center: [-111.0429, 45.67],
                       zoom: 10,
                       map: fishMap 
                   })
-                  var featureLayer = new FeatureLayer("https://services3.arcgis.com/Cdxz8r11hT0MGzg1/arcgis/rest/services/FWPLND_FAS_POINTS/FeatureServer/0");
+                  
+                  var popupTemplate = { // autocasts as new PopupTemplate()
+                    title: "FAP: {NAME}",
+                    content: "<p><b> Web: {WEB_PAGE}% </b></p>"
+                      // "<p> Married: {MARRIED_CY}</p>" +
+                      // "<p> Never Married: {NEVMARR_CY}</p>" +
+                      // "<p> Divorced: {DIVORCD_CY}</p>"
+                  };
+                  
+                  var featureLayer = new FeatureLayer({
+                    url: "https://services3.arcgis.com/Cdxz8r11hT0MGzg1/arcgis/rest/services/FWPLND_FAS_POINTS/FeatureServer/0",
+                    outFields: ['*'],
+                    popupTemplate: popupTemplate
+                  });
                   fishMap.add(featureLayer);
+
+                  fishView.whenLayerView(featureLayer).then(function(lyrView) {
+                    lyrView.watch("updating", function(val) {
+                      if (!val) { // wait for the layer view to finish updating
+          
+                        // query all the features available for drawing.
+                        lyrView.queryFeatures().then(function(results) {
+                            console.log(results);
+                          // graphics = results;
+          
+                          // var fragment = document.createDocumentFragment();
+          
+                          results.forEach(function(result, index) {
+                            var attributes = result.attributes;
+                            console.log(attributes.NAME + ", " + attributes.WEB_PAGE);
+                      
+                            // var name = attributes.ZIP + " (" +
+                            //   attributes.PO_NAME + ")"
+          
+                          //   // Create a list zip codes in NY
+                          //   var li = document.createElement("li");
+                          //   li.classList.add("panel-result");
+                          //   li.tabIndex = 0;
+                          //   li.setAttribute("data-result-id", index);
+                          //   li.textContent = name;
+          
+                          //   fragment.appendChild(li);
+                          });
+                        //   // Empty the current list
+                        //   listNode.innerHTML = "";
+                        //   listNode.appendChild(fragment);
+                         });
+                      }
+                    });
+                  });
+
                 }}
                 
               onError={error => console.error(error)}
