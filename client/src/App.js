b6b9ced8b04e45185225268dceee4964be236b60
Emily Yost <esyost@gmail.com>
2017-12-09 17:57:37 -0700

    cleaned up indentations on two files -- noticed our navbar does not show a hamburger

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     }
     this.latLongCallback = this.latLongCallback.bind(this);
   }
+
   latLongCallback(clickedFap) {
     this.setState({
       clickedFapLat: clickedFap.clickedFapLat,
@@ -24,63 +25,60 @@ class App extends Component {
   }
 
   render() {
-    
-        return (
-            <header className="bgimage img-responsive">
-            
-            {/* <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">          */}
-              
-              <nav className="navbar navbarstyle navbar-fixed-top">
-              <div className="container">
-                <div className="navbar-header">
-                  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                  </button>
-                  <a className="navbar-brand" href="">Gillz-N-Skillz</a>
-                </div>
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                 
-                  <ul className="nav navbar-nav navbar-right">
-                    <li><a href="">Link</a></li>
-                    <li className="dropdown">
-                      <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                      <ul className="dropdown-menu">
-                        <li><a href="">My Favorites</a></li>
-                        <li><a href="">Current Conditions</a></li>
-                        <li><a href="">Logout</a></li>
-                  </ul>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-          
-          <div className="container map">
-           {/* <Well bsSize="large">  */}
-              <FishMap callbackFromApp={this.latLongCallback}/>
-              <br />
-          {/* </Well> */}
+    return (
+      <header className="bgimage img-responsive">
+        {/* <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">          */}
+        <nav className="navbar navbarstyle navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="">Gillz-N-Skillz</a>
             </div>
-       
-         <div className="container">
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="">Link</a></li>
+                <li className="dropdown">
+                  <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                  <ul className="dropdown-menu">
+                    <li><a href="">My Favorites</a></li>
+                    <li><a href="">Current Conditions</a></li>
+                    <li><a href="">Logout</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="container-fluid map">
+          <FishMap callbackFromApp={this.latLongCallback} />
+          <br />
+        </div>
+
+        <div className="container-fluid">
           <div className="jumbotron">
             <h3>Site Details</h3>
             <p>I am your location. Check me out for directions, shops and post-fishing extravaganzas!</p>
           </div>
-          </div>
-          <div className="container">
-            <FapDetails fapDetails={this.state}/>
-          </div>
+        </div>
+
+        <div className="container-fluid">
+          <FapDetails fapDetails={this.state} />
+        </div>
+
+        <div className="container-fluid">
           <p className="caps">Copyright Gillz-n-Skillz 2017</p>
-          
+        </div>
+
       </header>
-      
-          
-        );
-      }
-    }
+    );
+  }
+}
 
 export default App;
--- client/src/components/fapDetails/FapDetails.js
+++ client/src/components/fapDetails/FapDetails.js
@@ -128,69 +128,77 @@ class FapDetails extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-      <div className="container-fluid">
-        <ul className="nav nav-tabs nav-justifed navtabdetails">
-          <li className="active"><a data-toggle="tab" href="#CurrentConditions">Current Conditions</a></li>
-          <li><a data-toggle="tab" href="#Wind">Wind</a></li>
-          <li><a data-toggle="tab" href="#Streamflow">Streamflow</a></li>
-          <li><a data-toggle="tab" href="#StreamTemperature">Stream Temperature</a></li>
-          <li><a data-toggle="tab" href="#Forecast">Forecast</a></li>
-        </ul>
+      // <div className="container-fluid">
 
-      <div className="tab-content tabcontentstyle">
-        <div id="CurrentConditions" className="tab-pane fade in active">
-          {/* <h3>Current Conditions</h3> */}
-          <i className={`wu wu-black wu-64 wu-${this.state.icon}`}></i>
-            <p>{this.state.weather}</p>
-            <p>{this.state.temp_f}℉</p>
-            <p>feels like {this.state.feelslike_f}℉</p>
-            <p>{this.state.precip_today_in} inches today</p>
-        </div>
-        <div id="Streamflow" className="tab-pane fade">
-            {/* <h3>Streamflow</h3> */}
+        <div>
+          <ul className="nav nav-tabs nav-justifed navtabdetails">
+            <li className="active"><a data-toggle="tab" href="#CurrentConditions">Current Conditions</a></li>
+            <li><a data-toggle="tab" href="#Wind">Wind</a></li>
+            <li><a data-toggle="tab" href="#Streamflow">Streamflow</a></li>
+            <li><a data-toggle="tab" href="#StreamTemperature">Stream Temperature</a></li>
+            <li><a data-toggle="tab" href="#Forecast">Forecast</a></li>
+          </ul>
+
+          <div className="tab-content tabcontentstyle">
+            <div id="CurrentConditions" className="tab-pane fade in active">
+              {/* <h3>Current Conditions</h3> */}
+              <i className={`wu wu-black wu-64 wu-${this.state.icon}`}></i>
+                <p>{this.state.weather}</p>
+                <p>{this.state.temp_f}℉</p>
+                <p>feels like {this.state.feelslike_f}℉</p>
+                <p>{this.state.precip_today_in} inches today</p>
+            </div>
+
+            <div id="Streamflow" className="tab-pane fade">
+              {/* <h3>Streamflow</h3> */}
               <p><img className="img-responsive center-block" src="https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=06190540&parm_cd=00060&period=7" alt="A graph courtesy of the US Geological Survey of discharge in cubic feet per second for the last seven days and the median daily statistic"/></p>
-         </div>
-        <div id="Wind" className="tab-pane fade">
-          {/* <h3>Wind</h3> */}
-            <p>{this.state.wind_mph} mph {this.state.wind_dir}</p>
-            <p>{this.state.wind_gust_mph}  mph gusts</p>
-        </div>
-        <div id="StreamTemperature" className="tab-pane fade">
-          {/* <h3>Stream Temperature</h3> */}
-          <p><img className="img-responsive center-block" src="https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=06190540&parm_cd=00010&period=7" alt="A graph courtesy of the US Geological Survey of water temperature in degrees Celsius and Fahrenheit for the last seven days"/></p>
-        </div>
-        <div id="Forecast" className="tab-pane fade">
-          {/* <h3>Forecast</h3> */}
-          <div className="container-fluid">
-            <div className="row">
-              <div class="col-xs-6 col-sm-4">
-                <p className="dayOfWeek">{this.state.forecastDay0Weekday}</p>            
-                <p><i className={`wu wu-black wu-64 wu-${this.state.forecastDay0Icon}`}></i></p>
-                <p>{this.state.forecastDay0Conditions}</p>
-                <p>{this.state.forecastDay0High}℉ / {this.state.forecastDay0Low}℉</p>
-                <p className="caps">high / low</p>
-              </div>
-              <div class="col-xs-6 col-sm-4">
-                <p className="dayOfWeek">{this.state.forecastDay1Weekday}</p>
-                <p><i className={`wu wu-black wu-64 wu-${this.state.forecastDay1Icon}`}></i></p>
-                <p>{this.state.forecastDay1Conditions}</p>
-                <p>{this.state.forecastDay1High}℉ / {this.state.forecastDay1Low}℉</p>
-                <p className="caps">high / low</p>
-              </div>
-              <div class="col-xs-6 col-sm-4">
-                <p className="dayOfWeek">{this.state.forecastDay2Weekday}</p>
-                <p><i className={`wu wu-black wu-64 wu-${this.state.forecastDay2Icon}`}></i></p>
-                <p>{this.state.forecastDay2Conditions}</p>
-                <p>{this.state.forecastDay2High}℉ / {this.state.forecastDay2Low}℉</p>
-                <p className="caps">high / low</p>
+            </div>
+
+            <div id="Wind" className="tab-pane fade">
+              {/* <h3>Wind</h3> */}
+              <p>{this.state.wind_mph} mph {this.state.wind_dir}</p>
+              <p>{this.state.wind_gust_mph}  mph gusts</p>
+            </div>
+
+            <div id="StreamTemperature" className="tab-pane fade">
+              {/* <h3>Stream Temperature</h3> */}
+              <p><img className="img-responsive center-block" src="https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=06190540&parm_cd=00010&period=7" alt="A graph courtesy of the US Geological Survey of water temperature in degrees Celsius and Fahrenheit for the last seven days"/></p>
+            </div>
+
+            <div id="Forecast" className="tab-pane fade">
+              {/* <h3>Forecast</h3> */}
+              <div className="container-fluid">
+                <div className="row">
+                  <div class="col-xs-6 col-sm-4">
+                    <p className="dayOfWeek">{this.state.forecastDay0Weekday}</p>            
+                    <p><i className={`wu wu-black wu-64 wu-${this.state.forecastDay0Icon}`}></i></p>
+                    <p>{this.state.forecastDay0Conditions}</p>
+                    <p>{this.state.forecastDay0High}℉ / {this.state.forecastDay0Low}℉</p>
+                    <p className="caps">high / low</p>
+                  </div>
+
+                  <div class="col-xs-6 col-sm-4">
+                    <p className="dayOfWeek">{this.state.forecastDay1Weekday}</p>
+                    <p><i className={`wu wu-black wu-64 wu-${this.state.forecastDay1Icon}`}></i></p>
+                    <p>{this.state.forecastDay1Conditions}</p>
+                    <p>{this.state.forecastDay1High}℉ / {this.state.forecastDay1Low}℉</p>
+                    <p className="caps">high / low</p>
+                  </div>
+
+                  <div class="col-xs-6 col-sm-4">
+                    <p className="dayOfWeek">{this.state.forecastDay2Weekday}</p>
+                    <p><i className={`wu wu-black wu-64 wu-${this.state.forecastDay2Icon}`}></i></p>
+                    <p>{this.state.forecastDay2Conditions}</p>
+                    <p>{this.state.forecastDay2High}℉ / {this.state.forecastDay2Low}℉</p>
+                    <p className="caps">high / low</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+
           </div>
         </div>
-      </div>
-      </div>
+      // </div>
     );
   }
 }
