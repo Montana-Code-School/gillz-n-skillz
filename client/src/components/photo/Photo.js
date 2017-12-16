103433626b37de5779a879f0097dc45e3d75c070
Jill <jilliandmartin@gmail.com>
2017-12-16 15:04:14 -0700

    flickr photos work not getting data anymore

--- client/src/components/photo/Photo.js
+++ client/src/components/photo/Photo.js
@@ -14,50 +14,36 @@ class Photo extends Component {
     };
   }
 
-//Leaving if need to mount to state
-  componentDidMount() {
-    this.showPhotos()
-  }
-
-
-//Leaving is need to send props...
   // componentWillReceiveProps() {
   //   this.drawWeather(this.props.fapDetails.clickedFapLat, this.props.fapDetails.clickedFapLong, this.props.fapDetails.clickedFapSiteId)
   // }
 
-  showPhotos() {
-    axios.get(
-      //change to flickr api
-      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=045b246c90125977977e4ba75b178eda&tags=fish%2C+montana%2C+christmas&format=json&nojsoncallback=1&auth_token=72157690347740604-52cc3453ea361f2e&api_sig=35ab1b7934f1d256066df524d9361477"      //double check query format
-      // + lat
-      // + "," 
-      // + long 
-      // + ".json"
-      
+  displayPhotos() {
+  axios.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dd0b51e5691f1ff424582e8989e89e80&tags=montana%2C+fly+fishing&format=json&nojsoncallback=1&api_sig=17daeda86f1ece723395393a261c8c2a"     
     )
-    
-      .then((response) => {
-        this.setState({
-          //need to target the photo url to display
-          photo: response.data.photos.photo[0].id
-          
-        })
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
+    .then((response) => {
+      console.log(response);
+    //   this.setState({
+    //     photo: response.data.items[0].media.m
+    //     })
+    //     console.log(response.data);
+    })
+    .catch((err) => {
+    console.log(err)
+    })
+}
 
-    render() {
-      return (
-        <div className="container-fluid">
+  render() {
+    return (
+      <div className="container-fluid">
         <div className="jumbotron">
-        <p> {this.state.photo} </p>
-        </div>
-        </div>
-      );
-    }  
+       <p> hello</p>
+        <p>{this.state.photo}</p>
+      </div>
+      </div>
+    );
+  }
+
 
 }
 
