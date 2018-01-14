483b4b10040896527172e57a0a62b33571def22e
Emily Yost <esyost@gmail.com>
2018-01-13 21:13:15 -0700

    Registration page form matches CSS

--- client/src/components/registration/Registration.js
+++ client/src/components/registration/Registration.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../header/Header';
 import axios from 'axios';
 
@@ -13,34 +14,34 @@ class Registration extends Component {
       lastname: "",
       email: "",
       licenseNo: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
-  this.handleChange = this.handleChange.bind(this);
-}
 
-handleChange(event) {
-  this.setState({[event.target.name]: event.target.value});
-}
-
-handleSubmit(event){
-  event.preventDefault();
-  const userRegistration = {
-    username: this.state.username,
-    password: this.state.password,
-    firstname: this.state.firstname,
-    lastname: this.state.lastname,
-    email: this.state.email,
-    licenseNo: this.state.licenseNo
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
-  
-  axios.post('/api/anglers', userRegistration) 
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const userRegistration = {
+      username: this.state.username,
+      password: this.state.password,
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      licenseNo: this.state.licenseNo
+    }
+
+    axios.post('/api/anglers', userRegistration)
       .then((res) => {
-          const userLogin = {
-            username: this.state.username,
-            password: this.state.password
-          }
+        const userLogin = {
+          username: this.state.username,
+          password: this.state.password
+        }
         //use AT to login a new user once registered
-          axios.post('/api/anglers/login', userLogin)
-          .then ((res) => {
+        axios.post('/api/anglers/login', userLogin)
+          .then((res) => {
             localStorage.setItem("gillznskillzAT", res.data.id)
             this.props.history.push('/angler/me');
           })
@@ -51,44 +52,83 @@ handleSubmit(event){
       .catch((error) => {
         alert("Can't register.");
       })
-}
+  }
 
-// <p> <a href="https://app.mt.gov/als/index/index.html" target="blank"> Need a License? </a></p>
-
- render () {
-  return (
-    <div className="bgimage img-responsive">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-12">
-            <Header history={this.props.history}/>
-          </div>
-        </div>
+  render() {
+    return (
+      <div className="bgimage img-responsive">
         <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="jumbotron">
-                <div className="form-group">
-                  <form className="form-signin" onSubmit={(e) => this.handleSubmit(e)}>
-                    <h2 className="form-signin-heading">Create an Account</h2>
-                      <label>Username:<input type="text" name="username" onChange={this.handleChange} /><br /></label>
-                      <label>Password:<input type="password" name="password" onChange={this.handleChange} /><br /></label>
-                      <label>First Name:<input type="text" name="firstname" onChange={this.handleChange} /><br /></label>
-                      <label>Last Name:<input type="text" name="lastname" onChange={this.handleChange} /><br /></label>
-                      <label>Email:<input type="text" name="email" onChange={this.handleChange} /><br /></label>
-                      <label>Fishing License No.:<input type="text" name="licenseNo" onChange={this.handleChange} /><br /></label>
-                      <button className="btn btn-lg btn-primary btn-block" type="submit"> Register </button>
+          <div className="row">
+            <div className="col-lg-12">
+              <Header history={this.props.history} />
+            </div>
+          </div>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-4"></div>
+              <div className="col-md-4">
+                <div className="well">
+                  <form class="form-horizontal">
+                    <fieldset>
+                      <form className="form-signin" onSubmit={(e) => this.handleSubmit(e)}>
+                        <div className="form-group">
+                          <legend>Create an Account</legend>
+                          <div className="form-group">
+                            <label className="col-lg-2 control-label">Username</label>
+                            <div class="col-lg-10">
+                              <input type="text" className="form-control" name="username" placeholder="Username" onChange={this.handleChange} />
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <label className="col-lg-2 control-label">Password</label>
+                            <div class="col-lg-10">
+                              <input type="text" className="form-control" name="password" placeholder="Password" onChange={this.handleChange} />
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <label className="col-lg-2 control-label">First Name</label>
+                            <div class="col-lg-10">
+                              <input type="text" className="form-control" name="firstname" placeholder="First Name" onChange={this.handleChange} />
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <label className="col-lg-2 control-label">Last Name</label>
+                            <div class="col-lg-10">
+                              <input type="text" className="form-control" name="lastname" placeholder="Last Name" onChange={this.handleChange} />
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <label className="col-lg-2 control-label">Email</label>
+                            <div class="col-lg-10">
+                              <input type="text" className="form-control" name="email" placeholder="Email" onChange={this.handleChange} />
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <label className="col-lg-2 control-label">Fishing License No. </label> 
+                            <div class="col-lg-10">
+                              <input type="text" className="form-control" name="licenseNo" placeholder="Fishing License No." onChange={this.handleChange} />
+                              <span class="help-block"><a href="https://app.mt.gov/als/index/index.html" target="blank"> Need a License? </a></span>
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <div class="col-lg-10">
+                              <Button className="btn-info" type="submit"> Register </Button>
+                              <Link className="btn btn-default" to="/login">Back to Login</Link>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </fieldset>
                   </form>
-                  <p><Link to="/login">Back to Login</Link></p>
                 </div>
               </div>
             </div>
           </div>
+          <div className="col-md-4"></div>
         </div>
-      </div>
-    </div>
-   );
-  } 
+      </div >
+    );
+  }
 }
 
- export default Registration;
+export default Registration;
