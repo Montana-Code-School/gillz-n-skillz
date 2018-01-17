7a8e677b3b1eb22b66aef5cc3adff31d28734bca
Jill <jilliandmartin@gmail.com>
2018-01-16 20:06:38 -0700

    fish photo tweaks

--- client/src/components/fishPhotos/FishPhotos.js
+++ client/src/components/fishPhotos/FishPhotos.js
@@ -25,30 +25,11 @@ componentDidMount() {
     this.displayFishPhotos("5", "4595", "24430478167", "37377f0367")
   }
 
-// use this to display multiple?
-// componentWillReceiveProps(nextProps) {
-//   this.setState ({
-//     farmId: nextProps.fishPhotos.farmId,
-//     serverId: nextProps.fishPhotos.serverId,
-//     id: nextProps.fishPhotos.id,
-//     secret: nextProps.fishPhotos.secret,
-//     fishPic: <img src={`https://farm${this.state.farmId}.staticflickr.com/${this.state.serverId}/${this.state.id}_${this.state.secret}.jpg`} alt=""/>
-//   })
-//   this.displayFishPhotos(this.state.fishPic)
-// }
-
-// flickr url format https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-// display fishing photos on home page below map using flickr api
   displayFishPhotos(farmId, serverId, id, secret) {
   axios.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4dff2ac6b49370264bff812fd6c159eb&tags=fly%20fishing%20montana&tag_mode=AND&format=json&nojsoncallback=1")
     .then((response) => {
-      //loop through data response to display photos using farm, server, id and secret
+      console.log(response);
       this.setState({
-        // farmId: response.data.photos.photo[0].farm,
-        // serverId: response.data.photos.photo[0].server,
-        // id: response.data.photos.photo[0].id,
-        // secret: response.data.photos.photo[0].secret,
-//fishPicArray will display with fresh API key
         fishPicArray: response.data.photos.photo
       })
     })
@@ -58,10 +39,10 @@ componentDidMount() {
 }
 
   render() {
-    // console.log(this.state.fishPicArray)
+    console.log(this.state.fishPicArray)
     let fishItems = this.state.fishPicArray.map((trout) => {
       return (
-        <img className="img-responsive fishphotos" key={trout.id} src={`https://farm${trout.farm}.staticflickr.com/${trout.server}/${trout.id}_${trout.secret}.jpg`} alt=""/>
+        <img className="img-responsive fishphotos thumbnail" key={trout.id} src={`https://farm${trout.farm}.staticflickr.com/${trout.server}/${trout.id}_${trout.secret}.jpg`} alt=""/>
       )
     });
     return (
@@ -69,24 +50,24 @@ componentDidMount() {
         <div className="jumbotron">
           <div className="row">
             <div className="col-md-3">
-              <div className="thumbnail img-responsive">
+              
               {fishItems[1]}
-              </div>
+              
             </div>
             <div className="col-md-3">
-            <div className="thumbnail img-responsive">
+            
               {fishItems[2]}
-              </div>
+             
             </div>
             <div className="col-md-3">
-            <div className="thumbnail img-responsive">
+           
               {fishItems[3]}
-            </div>
+            
             </div>
             <div className="col-md-3">
-            <div className="thumbnail img-responsive">
+           
               {fishItems[4]}
-              </div>
+              
             </div> 
          </div>
          </div>
